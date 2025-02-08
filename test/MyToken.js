e05c02723cb065a2d4881fb50c30d6312f528d3b
Sainath More <sainathmore501@gmail.com>
2025-02-08 16:53:46 +0530

    Added Hardhat project files

---
+++ test/MyToken.js
@@ -0,0 +1,13 @@
+const { expect } = require("chai");
+
+describe("MyToken", function () {
+  it("Should deploy and mint initial supply", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("MyToken");
+    const token = await Token.deploy(owner.address);
+    await token.deployed();
+
+    expect(await token.balanceOf(owner.address)).to.equal(ethers.utils.parseUnits("1000000000", 18));
+  });
+});
